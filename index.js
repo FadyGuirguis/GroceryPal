2d74472d7d886eed79c5cf25e145efd74e861842
Fady Sameh <fady97sameh@gmail.com>
2018-11-23 01:30:18 +0200

    adding cors and body-parser

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 let express = require('express');
+let bodyParser = require('body-parser');
+let cors = require('cors');
 
 require('./config/config.js');
 
 let app = express();
+
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+    methods: ['GET', 'POST', 'PATCH', 'DELETE']
+  })
+);
+
+app.use(bodyParser.urlencoded({ extended: false })); // Parses urlencoded bodies
+app.use(bodyParser.json());
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
