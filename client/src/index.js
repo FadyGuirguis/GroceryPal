e7b69addedd02255b49faedd5f4c1c9191b3c313
Fady Sameh <fady97sameh@gmail.com>
2018-12-01 12:39:02 +0200

    redux setup

--- client/src/index.js
+++ client/src/index.js
@@ -5,11 +5,16 @@ import App from './App.jsx';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
+import configStore from './store/configStore';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(            
+ReactDOM.render(
+  <Provider store={configStore()}>
     <BrowserRouter>
-         <App />
-    </BrowserRouter>               
-    , document.getElementById('root'));
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
+);
                 
 registerServiceWorker();
